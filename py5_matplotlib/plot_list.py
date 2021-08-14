81151cf734b9f92c9735fabe2f5a0ffeed8a2a66
TebanRiveros <seriverosm@correo.udistrital.edu.co>
2021-08-13 23:12:14 -0500

    Algoritmo lista

--- py5_matplotlib/plot_list.py
+++ py5_matplotlib/plot_list.py
@@ -1,9 +1,15 @@
-import matplotlib
+lista =[]
+numero = int(input('ingrese el numero a evaluar :'))
+lista.append(numero)
 
-numero=input("Escriba un numero:")
-lista=[numero]
+
 while numero != 1:
-  if (numero%2)==0:
-    nuevo=numero/2
-    lista
-  else:
+    c = numero % 2
+    if c == 0:
+        numero=numero/2
+        lista.append(numero)
+    else:
+        numero= numero *3 +1
+        lista.append(numero)
+
+print(f"la lista es: { lista }")
